c82847e39428d7791445b8cb792da58ea752c673
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-19 15:06:46 +0000

    final edits

--- src/main.js
+++ src/main.js
@@ -2,20 +2,18 @@
 import "bootstrap";
 import "./styles.css";
 
-window.onload = function() {
+window.onload = () => {
   let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
   let suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
+
   function randomSuit() {
-    return suits[Math.floor(Math.random()) * suits.length];
+    return suits[Math.floor(Math.random() * suits.length)];
   }
   function randomNumber() {
-    return numbers[Math.floor(Math.random()) * numbers.length];
+    return numbers[Math.floor(Math.random() * numbers.length)];
   }
-  console.log();
+  var pickACard = randomSuit();
+  document.querySelector(".top").innerHTML = pickACard;
+  document.querySelector(".number").innerHTML = randomNumber();
+  document.querySelector(".bottom").innerHTML = pickACard;
 };
-
-document.querySelector(".top").innerHTML = "&#9824;";
-
-document.querySelector(".number").innerHTML = "5";
-
-document.querySelector(".bottom").innerHTML = "&#9824;";
