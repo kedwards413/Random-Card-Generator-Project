373330aadaba2cdcbe5daa245039e185e1dc864c
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-19 01:17:06 +0000

    design

--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-window.onload = function () {
-let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
-let suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
+window.onload = function() {
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+  let suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    console.log()
+  console.log();
 };
