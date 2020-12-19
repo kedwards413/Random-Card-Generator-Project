6834d26df205ef677718ef2d9b6ddd807071c625
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-19 14:48:48 +0000

    style updates

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import "./styles.css";
 window.onload = function() {
   let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
   let suits = ["&hearts;", "&diams;", "&spades;", "&clubs;"];
-
+  function randomSuit() {
+    return suits[Math.floor(Math.random()) * suits.length];
+  }
+  function randomNumber() {
+    return numbers[Math.floor(Math.random()) * numbers.length];
+  }
   console.log();
 };
 
